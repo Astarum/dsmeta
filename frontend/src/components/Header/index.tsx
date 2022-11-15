bc7ac4fc173f599972bf1bf30ae8a44a896217dd
Astarum <thauas_viana@hotmail.com>
2022-11-15 11:42:42 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+
+import logo from '../../assets/logo.svg'
+import './style.css'
+function Header() {
+    return (
+       <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta"></img>
+            <h1>DSMeta</h1>
+            <p>
+                Desenvolvido por
+                <a href="https://www.instagram.com/devsuperior.ig">@devsuperior</a>
+            </p>
+        </div>
+
+       </header>
+    )
+}
+
+export default Header
